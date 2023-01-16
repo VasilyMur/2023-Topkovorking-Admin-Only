eaef6f42aafe0c8edc410aaf2c891ce1cb8ad432
vasilymur <vmouravi@gmail.com>
2023-01-16 17:25:49 +0300

    main page update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -49,127 +49,32 @@ const Header: FC = () => {
           </a>
         </Link>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <a onClick={menuToggle}>
-              <Menu />
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <SwipeableDrawer
-        className={styles.mobileMenu}
-        anchor="right"
-        open={open}
-        // open
-        onOpen={menuToggle}
-        onClose={menuToggle}
-        PaperProps={{
-          sx: {
-            width: '360px',
-            height: 'calc(-24px + 100vh)',
-            marginTop: '12px',
-            borderRadius: '12px 0 0 12px'
-          }
-        }}
-      >
-        <Flexbox
-          flexDirection="column"
-          justifyContent="space-between"
-          height="100%"
-        >
-          <Flexbox flexDirection="column">
-            <Flexbox className={styles.mobileMenuHeader}>
-              <Link href={{ pathname: '/' }}>
-                <a className={styles.headerImgLink}>
-                  <Image src="/crown.png" width={25} height={25} />
-                  <Typography variant="body1">Topkovorking.ru</Typography>
-                </a>
-              </Link>
-              <Close onClick={menuToggle} />
-            </Flexbox>
-
-            <Flexbox className={styles.mobileMenuContent}>
-              <Flexbox className={styles.mobileMenuContentCities}>
-                <Link href={{ pathname: '/' }}>
-                  <a>
-                    <Typography variant="body2">Москва</Typography>
-                  </a>
-                </Link>
-                <Link
-                  as={`/city/${CITY_NAME_SPB}`}
-                  href={{
-                    pathname: `/city`,
-                    query: { city: CITY_NAME_SPB }
-                  }}
-                >
-                  <a>
-                    <Typography variant="body2">Санкт-Петербург</Typography>
-                  </a>
-                </Link>
-
-                <Link
-                  as={`/city/${CITY_NAME_EKB}`}
-                  href={{
-                    pathname: `/city`,
-                    query: { city: CITY_NAME_EKB }
-                  }}
-                >
-                  <a>
-                    <Typography variant="body2">Екатеринбург</Typography>
-                  </a>
-                </Link>
-
-                <Link
-                  as={`/city/${CITY_NAME_KAZAN}`}
-                  href={{
-                    pathname: `/city`,
-                    query: { city: CITY_NAME_KAZAN }
-                  }}
-                >
-                  <a>
-                    <Typography variant="body2">Казань</Typography>
-                  </a>
-                </Link>
-              </Flexbox>
-            </Flexbox>
+      {
+        state.isAuthorized ? (
+          <Flexbox>
+            <div>
+              <a onClick={handleLogout}>
+                <Button size="small" variant="outlined">
+                  Выйти
+                </Button>
+              </a>
+            </div>
           </Flexbox>
-          {state.isAuthorized ? (
-            <Flexbox className={styles.mobileMenuContentFooter}>
-              <div>
-                <Link href={{ pathname: '/admin' }}>
-                  <a>
-                    <Button size="small" variant="contained">
-                      Кабинет
-                    </Button>
-                  </a>
-                </Link>
-              </div>
-
-              <div>
-                <a onClick={handleLogout}>
-                  <Button size="small" variant="outlined">
-                    Выйти
-                  </Button>
-                </a>
-              </div>
-            </Flexbox>
-          ) : (
-            <Flexbox className={styles.mobileMenuContentFooter}>
-              <div>
-                <Link href={{ pathname: '/admin' }}>
-                  <a>
-                    <Button size="small" variant="outlined">
-                      Войти
-                    </Button>
-                  </a>
-                </Link>
-              </div>
-            </Flexbox>
-          )}
-        </Flexbox>
-      </SwipeableDrawer>
+        ) : null
+        // (
+        //   <Flexbox className={styles.mobileMenuContentFooter}>
+        //     <div>
+        //       <Link href={{ pathname: '/' }}>
+        //         <a>
+        //           <Button size="small" variant="outlined">
+        //             Войти
+        //           </Button>
+        //         </a>
+        //       </Link>
+        //     </div>
+        //   </Flexbox>
+        // )
+      }
     </header>
   );
 };
