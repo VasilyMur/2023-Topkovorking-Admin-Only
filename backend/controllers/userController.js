bff9e7d3b81e70e5eaebc4612f554ec11b3303eb
vasilymur <vmouravi@gmail.com>
2023-04-09 17:51:02 +0300

    script upgrade

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -257,42 +257,38 @@ exports.register = async (req, res, next) => {
                               if (!document.hidden && startTime <= endTime) {
                                 startTime = Date.now();
                                 totalTime += ONE_SECOND;
-                                document.getElementById("timer").innerHTML = formatTime(totalTime);
+                    
+                                console.log('totalTime >>>>> ', formatTime(totalTime));
                               }
                             }, ONE_SECOND);
 
                             document.addEventListener("DOMContentLoaded", function () {
-                              document.getElementById("page").innerHTML = window.location.pathname;
+                              console.log('window.location.pathname >>> ', window.location.pathname);
+                    
                               events.forEach(function (e) {
                                 document.addEventListener(e, function () {
                                   endTime = Date.now() + INTERVAL_WAIT;
                                   if (e === "mouseup") {
                                     clickCount++;
-                                    document.getElementById("click").innerHTML = clickCount;
                                     if (event.target.nodeName === 'BUTTON') {          
                                       if(!buttonClicks[event.target.innerText]){
                                         buttonClicks[event.target.innerText] = 0;
                                       }
                                       buttonClicks[event.target.innerText] += 1;
-                                      buttonClicks.total += 1;
-                                      document.getElementById("button").innerHTML = JSON.stringify(buttonClicks, null, 2);          
+                                      buttonClicks.total += 1;         
                                     }
                                     else if (event.target.nodeName === 'A') {
                                       linkClickCount++;
-                                      document.getElementById("link").innerHTML = linkClickCount;
                                     }
                                   }
                                   else if (e === "keydown") {
                                     keypressCount++;
-                                    document.getElementById("keypress").innerHTML = keypressCount;
                                   }
                                   else if (e === "scroll") {
                                     scrollCount++;
-                                    document.getElementById("scroll").innerHTML = scrollCount;
                                   }
                                   else if (e === "mousemove") {
                                     mouseMovementCount++;
-                                    document.getElementById("mouse").innerHTML = mouseMovementCount;
                                   }
                                 });
                               });
--- test.js
+++ test.js
@@ -43,51 +43,49 @@
 //           if (!document.hidden && startTime <= endTime) {
 //             startTime = Date.now();
 //             totalTime += ONE_SECOND;
-//             document.getElementById("timer").innerHTML = formatTime(totalTime);
+
+//             console.log('totalTime >>>>> ', formatTime(totalTime));
 //           }
 //         }, ONE_SECOND);
+
+//         function formatTime(ms) {
+//             return Math.floor(ms / 1000);
+//           }
       
 //         document.addEventListener("DOMContentLoaded", function () {
-//           document.getElementById("page").innerHTML = window.location.pathname;
+//           console.log('window.location.pathname >>> ', window.location.pathname);
+
 //           events.forEach(function (e) {
 //             document.addEventListener(e, function () {
 //               endTime = Date.now() + INTERVAL_WAIT;
 //               if (e === "mouseup") {
 //                 clickCount++;
-//                 document.getElementById("click").innerHTML = clickCount;
 //                 if (event.target.nodeName === 'BUTTON') {          
 //                   if(!buttonClicks[event.target.innerText]){
 //                     buttonClicks[event.target.innerText] = 0;
 //                   }
 //                   buttonClicks[event.target.innerText] += 1;
-//                   buttonClicks.total += 1;
-//                   document.getElementById("button").innerHTML = JSON.stringify(buttonClicks, null, 2);          
+//                   buttonClicks.total += 1;         
 //                 }
 //                 else if (event.target.nodeName === 'A') {
 //                   linkClickCount++;
-//                   document.getElementById("link").innerHTML = linkClickCount;
 //                 }
 //               }
 //               else if (e === "keydown") {
 //                 keypressCount++;
-//                 document.getElementById("keypress").innerHTML = keypressCount;
 //               }
 //               else if (e === "scroll") {
 //                 scrollCount++;
-//                 document.getElementById("scroll").innerHTML = scrollCount;
 //               }
 //               else if (e === "mousemove") {
 //                 mouseMovementCount++;
-//                 document.getElementById("mouse").innerHTML = mouseMovementCount;
 //               }
 //             });
 //           });
 //         });
       
       
-//         function formatTime(ms) {
-//           return Math.floor(ms / 1000);
-//         }
+
 
 //         console.log('a >> ', a);
 //         console.log('o >> ', o);
